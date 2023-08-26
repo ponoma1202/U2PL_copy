0e2a9aa670d5f79fd005142c4978b2376d6bcb16
Vasilisa Ponomarenko <ponoma@berkeley.edu>
2023-08-26 12:19:31 -0700

    Deleted comments in train sup no edits file

--- train_sup_no_edits.py
+++ train_sup_no_edits.py
@@ -10,7 +10,6 @@ from datetime import datetime
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-#import torch.distributed as dist
 import torch.nn.functional as F
 import yaml
 from tensorboardX import SummaryWriter
@@ -24,7 +23,6 @@ import shutil                                                   # For Mike's cod
 
 from u2pl.dataset.builder import get_loader
 from u2pl.models.model_helper import ModelBuilder
-#from u2pl.utils.dist_helper import setup_distributed
 from u2pl.utils.loss_helper import get_criterion
 from u2pl.utils.lr_helper import get_optimizer, get_scheduler
 from u2pl.utils.utils import (
@@ -39,10 +37,8 @@ from u2pl.utils.utils import (
 
 parser = argparse.ArgumentParser(description="Semi-Supervised Semantic Segmentation")
 parser.add_argument("--config", type=str, default="config.yaml")
-#parser.add_argument("--local_rank", type=int, default=0)
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--output_dirpath", type=str, default="./stats")
-#parser.add_argument("--port", default=None, type=int)
 
 
 def main():
@@ -51,8 +47,6 @@ def main():
     seed = args.seed
     cfg = yaml.load(open(args.config, "r"), Loader=yaml.Loader)
 
-    # logger = init_log("global", logging.INFO)
-    # logger.propagate = 0
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",  # Mike's logger
                         handlers=[logging.StreamHandler()])
@@ -63,17 +57,7 @@ def main():
     #cudnn.enabled = True
     #cudnn.benchmark = True
 
-    #rank, word_size = setup_distributed(port=args.port)
-
     rank = 0
-    # if rank == 0:
-    #     logger.info("{}".format(pprint.pformat(cfg)))
-    #     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    #     tb_logger = SummaryWriter(
-    #         osp.join(cfg["exp_path"], "log/events_seg/" + current_time)
-    #     )
-    # else:
-    #     tb_logger = None
 
     if args.seed is not None:
         print("set random seed to", args.seed)
@@ -95,14 +79,6 @@ def main():
 
     model.cuda()
 
-    # local_rank = int(os.environ["LOCAL_RANK"])
-    # model = torch.nn.parallel.DistributedDataParallel(
-    #     model,
-    #     device_ids=[local_rank],
-    #     output_device=local_rank,
-    #     find_unused_parameters=False,
-    # )
-
     criterion = get_criterion(cfg)
 
     train_loader_sup, val_loader = get_loader(cfg, seed=seed)
@@ -110,6 +86,7 @@ def main():
     # Optimizer and lr decay scheduler
     cfg_trainer = cfg["trainer"]
     cfg_optim = cfg_trainer["optimizer"]
+
     # TODO Get rid of line below and see what happens
     times = 10 if "pascal" in cfg["dataset"]["type"] else 1
 
@@ -143,12 +120,6 @@ def main():
     elif cfg["saver"].get("pretrain", False):
         load_state(cfg["saver"]["pretrain"], model, key="model_state")
 
-    # TODO: Delete
-    optimizer_old = get_optimizer(params_list, cfg_optim)
-    lr_scheduler = get_scheduler(
-        cfg_trainer, len(train_loader_sup), optimizer_old, start_epoch=last_epoch
-    )
-
     # start Mike's code
     if os.path.exists(args.output_dirpath):
         logging.info("Stats directory exists, deleting")
@@ -168,8 +139,6 @@ def main():
     # end Mike's code
 
     # Start to train model
-    #for epoch in range(last_epoch, cfg_trainer["epochs"]):
-
 
     # start Mike's code
     while not plateau_scheduler.is_done():
@@ -186,11 +155,9 @@ def main():
         train(
             model,
             optimizer,
-            plateau_scheduler,
             criterion,
             train_loader_sup,
             epoch,
-            #tb_logger,
             train_stats
         )
 
@@ -214,13 +181,6 @@ def main():
                 )
 
             torch.save(state, osp.join(cfg["saver"]["snapshot_dir"], "model-state-dict.pth"))
-
-            # logger.info(
-            #     "\033[31m * Currently, the best val result is: {:.2f}\033[0m".format(
-            #         best_prec * 100
-            #     )
-            # )
-            # tb_logger.add_scalar("mIoU val", prec, epoch)
 
             # start Mike's code
 
@@ -261,30 +221,20 @@ def main():
 def train(
     model,
     optimizer,
-    lr_scheduler,
     criterion,
     data_loader,
     epoch,
-    #tb_logger,
     train_stats
 ):
     model.train()
-
-    #data_loader.sampler.set_epoch(epoch)
-    data_loader_iter = iter(data_loader)
-
-    #rank, world_size = dist.get_rank(), dist.get_world_size()
-    rank = 0
 
     # TODO: replace these with train_stats parameters
     losses = AverageMeter(10)
     data_times = AverageMeter(10)
     batch_times = AverageMeter(10)
-    learning_rates = AverageMeter(10)
 
     # start Mike's code
     start_time = time.time()
-    #per_class_accuracy = []
     # end Mike's code
 
     batch_end = time.time()
@@ -292,12 +242,8 @@ def train(
         batch_start = time.time()
         data_times.update(batch_start - batch_end)
 
-        # lr = lr_scheduler.get_lr()
-        # learning_rates.update(lr[0])
-        # lr_scheduler.step()
-
         image, label = tensor_dict
-        batch_size, h, w = label.size()
+        batch_size, h, w = label.size()         
         image, label = image.cuda(), label.cuda()
         outs = model(image)
         pred = outs["pred"]
@@ -317,7 +263,6 @@ def train(
 
         # gather all loss from different gpus
         reduced_loss = loss.clone().detach()
-        #dist.all_reduce(reduced_loss)
         losses.update(reduced_loss.item())
 
         # start Mike's code
@@ -332,7 +277,7 @@ def train(
         batch_end = time.time()
         batch_times.update(batch_end - batch_start)
 
-        if step % 100 == 0 and rank == 0:
+        if step % 100 == 0:
             # start Mike's code
             cpu_mem_percent_used = psutil.virtual_memory().percent
             gpu_mem_percent_used, memory_total_info = get_gpu_memory()
@@ -341,7 +286,6 @@ def train(
 
             logging.info(
                 "Iter [{}/{}]\t"
-                #"Data {data_time.val:.2f} ({data_time.avg:.2f})\t"
                 "Time {batch_time.val:.2f} ({batch_time.avg:.2f})\t"
                 "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
                 "LR {lr.val:.5f} ({lr.avg:.5f})\t"
@@ -358,9 +302,6 @@ def train(
                     total_mem=memory_total_info
                 )
             )
-
-            #tb_logger.add_scalar("lr", learning_rates.avg, i_iter)
-            #tb_logger.add_scalar("Loss", losses.avg, i_iter)
 
     # start Mike's code
     train_stats.close_accumulate(epoch, 'train_loss', method='avg')  # this adds the avg loss to the train stats
@@ -395,13 +336,11 @@ def validate(
     # end Mike's code
 
     model.eval()
-    #data_loader.sampler.set_epoch(epoch)
 
     num_classes, ignore_label = (
         cfg["net"]["num_classes"],
         cfg["dataset"]["ignore_label"],
     )
-    #rank, world_size = dist.get_rank(), dist.get_world_size()
 
     intersection_meter = AverageMeter()
     union_meter = AverageMeter()
@@ -438,11 +377,6 @@ def validate(
         # gather all validation information
         reduced_intersection = torch.from_numpy(intersection).cuda()
         reduced_union = torch.from_numpy(union).cuda()
-        reduced_target = torch.from_numpy(target).cuda()
-
-        #dist.all_reduce(reduced_intersection)
-        #dist.all_reduce(reduced_union)
-        #dist.all_reduce(reduced_target)
 
         intersection_meter.update(reduced_intersection.cpu().numpy())
         union_meter.update(reduced_union.cpu().numpy())
@@ -455,7 +389,6 @@ def validate(
     train_stats.append_accumulate('iou', [metric for metric in iou_class])
     train_stats.close_accumulate(epoch, 'iou', method='avg')
     train_stats.close_accumulate(epoch, 'val_loss', method='avg')
-    # this adds the avg loss to the train stats
     train_stats.close_accumulate(epoch, 'val_accuracy', method='avg')
     train_stats.add(epoch, 'val_wall_time', time.time() - start_time)
 
